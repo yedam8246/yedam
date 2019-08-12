1979b1e2389f4e2c127cd09bd71012d3266775af
User <User@YD02-14>
2019-08-12 14:43:56 +0900

    a

--- 2048/src/view/StartController.java
+++ 2048/src/view/StartController.java
@@ -5,15 +5,15 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class StartController implements Initializable {
 
 	@FXML
-	private Image start; // 시작 버튼 선언
+	private ImageView start; // 시작 버튼 선언
 
 	void start() {
-
+		
 	}
 
 	@Override
