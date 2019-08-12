9c2539c992840cd6a425102dde64c07394fd878b
User <User@YD02-14>
2019-08-12 17:28:09 +0900

    a

--- 2048/src/game/Main.java
+++ 2048/src/game/Main.java
@@ -1,9 +1,48 @@
 package game;
 
-public class Main {
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+	
+	private Stage primaryStage;
+	private AnchorPane startLayout;
 	
 	public static void main(String[] args) {
+		launch(args);
 		
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+		primaryStage=stage;
+		
+		initStart();
+		
+	}
+	
+	public void initStart() {
+		
+		try {
+			startLayout=FXMLLoader.load(getClass().getResource("../view/Start.fxml"));
+			
+			Scene scene=new Scene(startLayout);
+			
+			scene.getRoot().requestFocus();
+			
+			primaryStage.setTitle("2048");
+			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- 2048/src/view/StartController.java
+++ 2048/src/view/StartController.java
@@ -6,15 +6,36 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 public class StartController implements Initializable {
 
 	@FXML
 	private ImageView start; // 시작 버튼 선언
-
-	void start() {
+	
+	@FXML
+	public void start(MouseEvent event) {
+		
+			System.out.println("clicked");
+		
 		
 	}
+	
+	
+	
+	@FXML
+	public void key(KeyEvent evt) {
+		
+		if(evt.getCode()==KeyCode.RIGHT) {
+			start.setX(start.getX()+10);
+		}else if(evt.getCode()==KeyCode.LEFT) {
+			start.setX(start.getX()-10);
+		}
+	}
+	
+	
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
